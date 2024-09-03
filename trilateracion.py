d3a8fce0184df68768cf0274d66055eaffbe6c64
Santiago Groba <santiagogroba@hotmail.com>
2024-09-03 15:22:33 -0300

    Ejercicio 2 - resuelto

---
+++ trilateracion.py
@@ -0,0 +1,112 @@
+import numpy as np
+from scipy.linalg import inv
+from scipy.interpolate import CubicSpline
+import matplotlib.pyplot as plt
+
+#parseo la ubicacion de los sensores [i, x_i(m), y_i(m), z_i(m)]
+sensor_pos = []
+with open('sensor_positions.txt', 'r') as file:
+    i=0
+    for line in file:
+        if i==0:
+            i+=1
+            continue
+        sensor_pos.append([float(x) for x in line.split(",")])
+        i+=1
+
+#parseo las distancias medidas [t(s), d1(m), d2(m), d3(m)]
+distance = []
+with open('measurements.txt', 'r') as file:
+    i=0
+    for line in file:
+        if i==0:
+            i+=1
+            continue
+        distance.append([float(x) for x in line.split(",")])
+        i+=1
+
+#parseo la ubicacion del objeto [t(s), x(m), y(m), z(m)] (ground truth)
+trajectory = []
+with open('trajectory.txt', 'r') as file:
+    i=0
+    for line in file:
+        if i==0:
+            i+=1
+            continue
+        trajectory.append([float(x) for x in line.split(",")])
+        i+=1
+
+def func(x, y, z, sensor_pos,distances,t):
+    actual_time = 0
+    for i in range(len(distances)):
+        if distances[i][0] == t:
+            actual_time = i
+            break
+    return np.array([
+        (x - sensor_pos[0][1])**2 + (y - sensor_pos[0][2])**2 + (z - sensor_pos[0][3])**2 - distances[actual_time][1]**2,
+        (x - sensor_pos[1][1])**2 + (y - sensor_pos[1][2])**2 + (z - sensor_pos[1][3])**2 - distances[actual_time][2]**2,
+        (x - sensor_pos[2][1])**2 + (y - sensor_pos[2][2])**2 + (z - sensor_pos[2][3])**2 - distances[actual_time][3]**2
+    ])
+
+def jacobian(x, y, z, sensor_pos):
+    return np.array([
+        [2*(x - sensor_pos[0][1]), 2*(y - sensor_pos[0][2]), 2*(z - sensor_pos[0][3])],
+        [2*(x - sensor_pos[1][1]), 2*(y - sensor_pos[1][2]), 2*(z - sensor_pos[1][3])],
+        [2*(x - sensor_pos[2][1]), 2*(y - sensor_pos[2][2]), 2*(z - sensor_pos[2][3])]
+    ])
+
+def newton_method(x0, y0, z0, sensor_pos,distances,t=0.0, tol=1e-6, max_iter=10000):
+    x, y, z = x0, y0, z0
+    for _ in range(max_iter):
+        f = func(x, y, z, sensor_pos,distances,t)
+        J = jacobian(x, y, z, sensor_pos)
+        
+        # Resolver el sistema J * delta = -f
+        delta = inv(J).dot(-f)
+        
+        # Actualizar la estimación
+        x, y, z = x + delta[0], y + delta[1], z + delta[2]
+        
+        # Comprobar la convergencia
+        if np.linalg.norm(delta) < tol:
+            return x, y, z
+    
+    raise ValueError("El método de Newton no convergió en el número máximo de iteraciones")
+
+
+# Estimación inicial
+x0, y0, z0 = 0, 0, 0
+
+#interpolacion de la trayectoria usando spline para 0.0 < t < 10
+t = 0.0
+trajectory_estimated = []
+for i in range(100):
+    x, y, z = newton_method(x0, y0, z0, sensor_pos,distance,t)
+    trajectory_estimated.append([t, x, y, z])
+    t += 0.5
+    x0, y0, z0 = x, y, z
+
+trajectory_estimated = np.array(trajectory_estimated)
+
+# Ahora ajusta splines a las coordenadas estimadas
+spline_x = CubicSpline(trajectory_estimated[:, 0], trajectory_estimated[:, 1])
+spline_y = CubicSpline(trajectory_estimated[:, 0], trajectory_estimated[:, 2])
+spline_z = CubicSpline(trajectory_estimated[:, 0], trajectory_estimated[:, 3])
+
+# Evaluar los splines en un rango más fino de tienpo, t0,tf,steps
+t_fine = np.linspace(0.0, 10.0, 100)  # Ajusta según tus necesidades
+x_fine = spline_x(t_fine)
+y_fine = spline_y(t_fine)
+z_fine = spline_z(t_fine)
+
+
+#plotear la trayectoria estimada y la real en 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot([x[1] for x in trajectory], [x[2] for x in trajectory], [x[3] for x in trajectory], label='Real')
+ax.plot(x_fine, y_fine, z_fine, label='Estimated')
+ax.legend()
+plt.show()
+
+
+
